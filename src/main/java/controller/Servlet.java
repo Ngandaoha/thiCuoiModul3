c579678b77d1e61e11eda7ee2349f93c65cf071c
Ngandaoha <158665295+Ngandaoha@users.noreply.github.com>
2024-10-04 00:16:29 +0700

    nop bai

--- src/main/java/controller/Servlet.java
+++ src/main/java/controller/Servlet.java
@@ -1,5 +1,6 @@
 package controller;
 
+import model.LoaiMatBang;
 import service.DAO;
 import service.IDAO;
 
@@ -9,11 +10,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
 
 @WebServlet(name="Servlet" ,urlPatterns = {"/", "/home"})
 public class Servlet extends HttpServlet {
 
-    private IDAO mainService;
+    private DAO mainService;
 
     public void init(){
         mainService = new DAO();
@@ -21,10 +24,23 @@ public class Servlet extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         request.setAttribute("list", mainService.findAll());
+        request.setAttribute("loaiMatBangs", mainService.findAllLoaiMatBang());
         request.getRequestDispatcher("list.jsp").forward(request, response);
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        int id = Integer.parseInt(request.getParameter("loaiMatBang"));
+        System.out.println(id);
+
+        if(id == 0){
+            request.setAttribute("list", mainService.findAll());
+            request.setAttribute("loaiMatBangs", mainService.findAllLoaiMatBang());
+            request.getRequestDispatcher("list.jsp").forward(request, response);
+        }
+        request.setAttribute("list", mainService.Search(id));
+        request.setAttribute("loaiMatBangs", mainService.findAllLoaiMatBang());
+        request.getRequestDispatcher("list.jsp").forward(request, response);
+        
         request.setCharacterEncoding("UTF-8");
         response.setCharacterEncoding("UTF-8");
     }
